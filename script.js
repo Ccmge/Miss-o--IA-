b9a51e1f1937db8c0472ac76fd72b2b814298a36
Ccmge <geovanna.paulowski@escola.pr.gov.br>
2026-09-17 12:04:10 -0300

    E

--- script.js
+++ script.js
@@ -88,7 +88,7 @@ function mostraAlternativas(){
 
 }
 function respostaSelecionada(opcaoSelecionada) {
-    const afirmacoes = opcaoSelecionada.afirmacao;
+    const afirmacoes = aleatorio( opcaoSelecionada.afirmacao);
     historiaFinal += afirmacoes + " ";
     atual++
     mostraPergunta();
