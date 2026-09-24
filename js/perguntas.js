74aac29b32fd5f000ab9fa23cb05b072fd90f9f7
Ccmge <geovanna.paulowski@escola.pr.gov.br>
2026-09-24 11:24:11 -0300

    kj

--- js/perguntas.js
+++ js/perguntas.js
@@ -1,4 +1,4 @@
-const perguntas = [
+export const perguntas = [
     {
         enunciado: "O cyberbullying é a violência virtual que ocorre geralmente com as pessoas tímidas e indefesas, ou simplesmente por não caírem na simpatia das mesmas. Como podemos evitá-lo",
         alternativas: [
@@ -6,12 +6,13 @@ const perguntas = [
                 texto: "Utilizando palestras para prevenir o cyberbullying nas escolas.",
                 afirmacao: "Você é uma pessoa reflexiva e busca aprendizado e conhecimento.",
                 "Você usa o seu pensamento crítico e o desejo de aprender para propor palestras escolares que combatem a violência virtual."
+            ]
             },
             {
                 texto:  "Prestando atenção aos sinais de cyberbullying nas escolas.",
-                afirmacao: "Você é uma pessoa observadora, pois se preocupa com o bem estar dos seus alunos.",
+                afirmacao: ["Você é uma pessoa observadora, pois se preocupa com o bem estar dos seus alunos.",
                 "Quem se importa de verdade com os alunos está sempre vigiando. O seu perfil observador é a chave para detectar o cyberbullying antes que ele cause danos."
-
+                ]
                          
             }    
            
@@ -41,13 +42,15 @@ const perguntas = [
             alternativas: [
                 {
                     texto: "Na maior parte do tempo, com certeza impacta, pois é a partir da autoestima que temos um olhar positivo ou negativo sobre nossas ações diárias. Sempre estou em busca de mais autoconhecimento.",
-                    afirmacao: "Você tem consciência que é importante ter uma boa auto-estima e procura se aprofundar mais sobre o assunto, vendo a vida de um jeito positivo.",
+                    afirmacao: ["Você tem consciência que é importante ter uma boa auto-estima e procura se aprofundar mais sobre o assunto, vendo a vida de um jeito positivo.",
                     "Você entende que a autoestima dita o ritmo dos nossos dias e, por isso, busca constantemente o autoconhecimento para enxergar a vida de forma mais leve e positiva."
+                    ]
                 },
                 {
                     texto:    "Apenas ocasionalmente ou raramente. Às vezes minha autoestima oscila e isso pode afetar negativamente minha qualidade de vida em certas ocasiões.",
-                    afirmacao: "Você demomostra que precisa se conhecer mais e mostra uma consciência de uma crescente necessidade de cuidar mais da própria percepção e bem-estar emocional.",
+                    afirmacao: ["Você demomostra que precisa se conhecer mais e mostra uma consciência de uma crescente necessidade de cuidar mais da própria percepção e bem-estar emocional.",
                     "Como a sua autoestima varia às vezes, você nota o impacto disso na sua rotina. Isso reflete um momento de amadurecimento, onde você busca se entender melhor e priorizar sua saúde mental."
+                    ]
                 }    
                
             ]
@@ -55,4 +58,4 @@ const perguntas = [
                
             ]
         },
-]
+];
